257f4c66ac0029bef28468912b246a59bc2da7c6
ZZX <2604533953@qq.com>
2021-07-08 21:15:43 +0800

    Add files via upload
    Softmax-Pooling-for-Super-Visual-Semantic-Embedding. Our code is based on VSE_inf.

---
+++ lib/modules/aggr/softpool.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+import torch
+import torch.nn as nn
+import math
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+def positional_encoding_1d(d_model, length):
+    """
+    :param d_model: dimension of the model
+    :param length: length of positions
+    :return: length*d_model position matrix
+    """
+    if d_model % 2 != 0:
+        raise ValueError("Cannot use sin/cos positional encoding with "
+                         "odd dim (got dim={:d})".format(d_model))
+    pe = torch.zeros(length, d_model)
+    position = torch.arange(0, length).unsqueeze(1)
+    div_term = torch.exp((torch.arange(0, d_model, 2, dtype=torch.float) *
+                          -(math.log(10000.0) / d_model)))
+    pe[:, 0::2] = torch.sin(position.float() * div_term)
+    pe[:, 1::2] = torch.cos(position.float() * div_term)
+
+    return pe
+
+class soft_max(nn.Module):
+    def __init__(self, d_pe, d_hidden):
+        super(soft_max, self).__init__()
+        self.d_pe = d_pe
+        self.d_hidden = d_hidden
+
+        self.pe_database = {}
+        self.soft = nn.Softmax(dim = 1)
+
+    def compute_pool_weights(self, lengths, features):
+        max_len = int(lengths.max())
+        pe_max_len = self.get_pe(max_len)
+        pes = pe_max_len.unsqueeze(0).repeat(lengths.size(0), 1, 1).to(lengths.device)
+        mask = torch.arange(max_len).expand(lengths.size(0), max_len).to(lengths.device)
+        mask = (mask < lengths.long().unsqueeze(1)).unsqueeze(-1)
+        weights = 0#torch.softmax(scores / 0.1, 1)
+        return weights, mask
+
+    def forward(self, features, lengths):
+        """
+        :param features: features with shape B x K x D
+        :param lengths: B x 1, specify the length of each data sample.
+        :return: pooled feature with shape B x D
+        """
+        pool_weights, mask = self.compute_pool_weights(lengths, features)
+
+        features = features[:, :int(lengths.max()), :]
+        features = features.masked_fill(mask == 0, -10000)
+        #sorted_features = sorted_features.sort(dim=1, descending=True)[0]
+        weights = self.soft(features / 0.01)
+
+        features = features.masked_fill(mask == 0, 0)
+
+        features = torch.mul(weights, features)
+
+        features = torch.sum(features, dim = 1)
+
+        #pooled_features = (sorted_features * pool_weights).sum(1)
+        return features, weights
+
+    def get_pe(self, length):
+        """
+
+        :param length: the length of the sequence
+        :return: the positional encoding of the given length
+        """
+        length = int(length)
+        if length in self.pe_database:
+            return self.pe_database[length]
+        else:
+            pe = positional_encoding_1d(self.d_pe, length)
+            self.pe_database[length] = pe
+            return pe
